00400ae692a3798caab8eed38387eb96f02778c3
Diego Carvalho (diegodmca) <diegodmca@outlook.com>
2022-09-20 08:59:49 -0300

    first commit

---
+++ src/app/segunda-pagina/segunda-pagina.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-segunda-pagina',
+  templateUrl: './segunda-pagina.component.html',
+  styleUrls: ['./segunda-pagina.component.css']
+})
+export class SegundaPaginaComponent {
+
+
+}
